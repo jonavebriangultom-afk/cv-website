ed2fb9d760375285feb55b855987146acf45fb8f
jonavebriangultom-afk <jonavebriangultom@gmail.com>
2026-02-08 16:02:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
+
+// menu mobile
+function toggleMenu() {
+    document.querySelector(".nav-menu").classList.toggle("active");
+}
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
